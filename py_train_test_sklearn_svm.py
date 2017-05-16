5a3d12ac5f6eff1a0d2af4e9109146c14b679398
Mani Kumar D A <manid2@users.noreply.github.com>
2017-05-16 11:38:25 +0530

    Code refactoring and documenting done.

--- py_train_test_sklearn_svm.py
+++ py_train_test_sklearn_svm.py
@@ -1,44 +1,86 @@
-import cv2, glob, random, math, numpy as np, dlib, itertools
+import cv2
+import glob
+import random
+import math
+import numpy as np
+import dlib
+import itertools
 from sklearn.svm import SVC
-__author__ = "Paul van Gent, 2016" #Please leave this line in
 
-#Change from:
-#emotions = ["neutral", "anger", "contempt", "disgust", "fear", "happy", "sadness", "surprise"] #Emotion list
+__original_author__ = "Paul van Gent, 2016"
+__author__ = "Mani Kumar D A, 2017"
 
-#To:
+""" 
+Mani experimenting with facial information extraction.
+
+@purpose: To extract all possible information from an image
+          and present it in json format for further processing.
+@applications: 1. Enhancing the multiple object detection in Computer Vision field.
+               2. Capturing a moment in the time based on the extracted information
+                  and applying auto filters to enhace the image.
+"""
+
+# Complete emotions lists:
+#emotions = ["neutral", "anger", "contempt", "disgust", "fear", "happy", "sadness", "surprise"]
+
+# Most important - positive emotions:
 emotions = ["happy", "surprise"]
 
 clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
 detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor("..\\shape_predictor_68_face_landmarks.dat") #Or set this to whatever you named the downloaded file
-clf = SVC(kernel='linear', probability=True, tol=1e-3)#, verbose = True) #Set the classifier as a support vector machines with polynomial kernel
+predictor = dlib.shape_predictor("..\\shape_predictor_68_face_landmarks.dat")
 
-def get_files(emotion): #Define function to get file list, randomly shuffle it and split 80/20
+# Set the classifier as a support vector machines with polynomial kernel
+clf = SVC(kernel='linear', probability=True, tol=1e-3)
+
+# Define function to get file list, randomly shuffle it and split 80/20
+def get_files(emotion):
     files = glob.glob("..\\dataset\\%s\\*" %emotion)
     random.shuffle(files)
     training = files[:int(len(files)*0.8)] #get first 80% of file list
     prediction = files[-int(len(files)*0.2):] #get last 20% of file list
     return training, prediction
 
+# Mani refactoring to create the constant 180 / pi
+# START
+rad2deg_const_factor = 180/math.pi
+# END - Mani refactor
+
 def get_landmarks(image):
     detections = detector(image, 1)
-    for k,d in enumerate(detections): #For all detected face instances individually
-        shape = predictor(image, d) #Draw Facial Landmarks with the predictor class
+    
+    # For all detected face instances individually
+    for k,d in enumerate(detections):
+        
+        # Draw Facial Landmarks with the predictor class
+        shape = predictor(image, d)
         xlist = []
         ylist = []
-        for i in range(1,68): #Store X and Y coordinates in two lists
+        # Store X and Y coordinates in two lists
+        for i in range(1,68):
             xlist.append(float(shape.part(i).x))
             ylist.append(float(shape.part(i).y))
-            
-        xmean = np.mean(xlist) #Get the mean of both axes to determine centre of gravity
+        
+        # Get the mean of both axes to determine centre of gravity
+        xmean = np.mean(xlist)
         ymean = np.mean(ylist)
-        xcentral = [(x-xmean) for x in xlist] #get distance between each point and the central point in both axes
+        
+        # Get distance between each point and the central point in both axes
+        xcentral = [(x-xmean) for x in xlist]
         ycentral = [(y-ymean) for y in ylist]
 
-        if xlist[26] == xlist[29]: #If x-coordinates of the set are the same, the angle is 0, catch to prevent 'divide by 0' error in function
+        # If x-coordinates of the set are the same, the angle is 0, catch to prevent 'divide by 0' error in function
+        if xlist[26] == xlist[29]:
             anglenose = 0
         else:
-            anglenose = int(math.atan((ylist[26]-ylist[29])/(xlist[26]-xlist[29]))*180/math.pi)
+            # Mani refactoring to enhance the readability and simplify the equation.
+            # START
+            # anglenose = int(math.atan((ylist[26]-ylist[29])/(xlist[26]-xlist[29]))*180/math.pi)
+            radians = math.atan( (ylist[26]-ylist[29]) / 
+                                 (xlist[26]-xlist[29]) )
+            # since degrees = radians * rad2deg_const_factor
+            anglenose = int(radians * rad2deg_const_factor)
+            # END - Mani refactor
 
         if anglenose < 0:
             anglenose += 90
@@ -67,16 +109,17 @@ def make_sets():
     prediction_labels = []
     for emotion in emotions:
         training, prediction = get_files(emotion)
-        #Append data to training and prediction list, and generate labels 0-7
+        # Append data to training and prediction list, and generate labels 0-7
         for item in training:
-            image = cv2.imread(item) #open image
-            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) #convert to grayscale
+            image = cv2.imread(item) # open image
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) # convert to grayscale
             clahe_image = clahe.apply(gray)
             landmarks_vectorised = get_landmarks(clahe_image)
             if landmarks_vectorised == "error":
                 pass
             else:
-                training_data.append(landmarks_vectorised) #append image array to training data list
+                # Append image array to training data list
+                training_data.append(landmarks_vectorised)
                 training_labels.append(emotions.index(emotion))
     
         for item in prediction:
@@ -94,18 +137,26 @@ def make_sets():
 
 accur_lin = []
 for i in range(0, 5):
-    print("Making sets %s" %i) #Make sets by random sampling 80/20%
+    # Make sets by random sampling 80/20%
+    print("Making sets %s" %i)
     training_data, training_labels, prediction_data, prediction_labels = make_sets()
 
-    npar_train = np.array(training_data) #Turn the training set into a numpy array for the classifier
+    # Turn the training set into a numpy array for the classifier
+    npar_train = np.array(training_data)
     npar_trainlabs = np.array(training_labels)
-    print("training SVM linear %s" %i) #train SVM
+    
+    # Train SVM
+    print("training SVM linear %s" %i)
     clf.fit(npar_train, training_labels)
 
-    print("getting accuracies %s" %i) #Use score() function to get accuracy
+    # Use score() function to get accuracy
+    print("getting accuracies %s" %i)
     npar_pred = np.array(prediction_data)
     pred_lin = clf.score(npar_pred, prediction_labels)
     print "linear: ", pred_lin
-    accur_lin.append(pred_lin) #Store accuracy in a list
+    
+    # Store accuracy in a list
+    accur_lin.append(pred_lin)
 
-print("Mean value lin svm: %.3f" %np.mean(accur_lin)) #Get mean accuracy of the 10 runs
+# Get mean accuracy of the i runs
+print("Mean value lin svm: %.3f" %np.mean(accur_lin))
