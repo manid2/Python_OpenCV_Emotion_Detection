7a9524ee0e07d365715a3e12496adca9b7149bb5
manid2 <maniyoung.d2@gmail.com>
2017-06-25 02:14:47 +0530

    Code refactoring phase 1.

--- src/py_experiment_features.py
+++ src/py_experiment_features.py
@@ -1,117 +1,166 @@
 """
-Mani experimenting with facial information extraction.
-@purpose:      To extract all possible information from an image
-               and present it in json or xml format for further processing.
-@applications: 1. Enhancing the multiple object detection in Computer Vision field.
-               2. Capturing a moment in the time based on the extracted information
-                  and applying auto filters to enhace the image.
-@Based on: <a href="http://www.paulvangent.com/2016/08/05/emotion-recognition-using-facial-landmarks/">
-              Emotion Recognition using Facial Landmarks, Python, DLib and OpenCV
-           </a>
+@introduction:
+------------------------------------------------------------------------------
+    Introduction
+    ============
+
+    This module is used to experiment and the test the feature generation
+    and data representation for SVM.
+------------------------------------------------------------------------------
+
+@usage:
+------------------------------------------------------------------------------
+    Usage
+    =====
+
+    Run the module as a command line option for python interpreter.
+    -> python py_experiment_features.py
+------------------------------------------------------------------------------
+
+@purpose:
+------------------------------------------------------------------------------
+    To understand the data, feature generation and representation, SVM model
+    training and SVM prediction.
+------------------------------------------------------------------------------
+
+@based_on:
+------------------------------------------------------------------------------
+    <a href="http://www.paulvangent.com/">
+       Emotion Recognition using Facial Landmarks, Python, DLib and OpenCV
+    </a>
+------------------------------------------------------------------------------
 """
 
+# ---------------------------------------------------------------------------
+# Imports
+# ---------------------------------------------------------------------------
+
+import os
 import cv2
 import dlib
 import math
 import numpy as np
 import datetime as dt
 
-# No need to modify this one as it is a helper script.
-__version__ = "1.0, 17/06/2017"
-__author__ = "Mani Kumar D A, Paul van Gent - 2016"
+# ---------------------------------------------------------------------------
+# Module info
+# ---------------------------------------------------------------------------
 
-# Set up some required objects
+__author__ = "Mani Kumar D A - 2017, Paul van Gent - 2016"
+__version__ = "2.1, 24/06/2017"
+__license__ = "GNU GPL v3"
+__copyright__ = "Mani Kumar D A"
+
+# ---------------------------------------------------------------------------
+# Module constants
+# ---------------------------------------------------------------------------
+
+dirsep = os.sep  # directory path separator
+curdir = os.curdir  # Relative current directory i.e. '.'
+cwdpath = os.getcwd()  # current working directory full path name
+claheObject = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
 video_capture = cv2.VideoCapture(0)  # Webcam object
-detector = dlib.get_frontal_face_detector()  # Face detector
+frontalFaceDetector = dlib.get_frontal_face_detector()
+facialShapePredictor = dlib.shape_predictor(
+    "..{0}input{1}shape_predictor_68_face_landmarks.dat".format(
+        dirsep, dirsep))
+RAD2DEG_CVT_FACTOR = 180 / math.pi  # Constant to convert radians to degrees.
 
-# Landmark identifier. Set the filename to whatever you named the
-# downloaded file
-predictor = dlib.shape_predictor("..\\input\\shape_predictor_68_face_landmarks.dat")
+# ---------------------------------------------------------------------------
+# Functions
+# ---------------------------------------------------------------------------
 
-# Constant factor to convert radians to degrees.
-rad2degCnvtFactor = 180 / math.pi
 
-while True:
-    ret, frame = video_capture.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
-    clahe_image = clahe.apply(gray)
-
-    detections = detector(clahe_image, 1)  # Detect the faces in the image
-
-    for k, d in enumerate(detections):  # For each detected face
-        shape = predictor(clahe_image, d)  # Get coordinates
-
-        xCoordinatesList = []
-        yCoordinatesList = []
-
-        # Store the X and Y coordinates of landmark points in two lists
-        for i in range(0, 68):
-            xCoordinatesList.append(shape.part(i).x)
-            yCoordinatesList.append(shape.part(i).y)
-
-        # Get the mean of both axes to determine centre of gravity
-        xCoordMean = np.mean(xCoordinatesList)
-        yCoordMean = np.mean(yCoordinatesList)
-        
-        # If x-coordinates of the set are the same, the angle is 0,
-        # catch to prevent 'divide by 0' error in the function
-        if xCoordinatesList[27] == xCoordinatesList[30]:
-            noseBridgeAngleOffset = 0
-            # radians1 = 1.5708 # 90 deg = 1.5708 rads
-        else:           
-            radians1 = math.atan(
-                (yCoordinatesList[27] - yCoordinatesList[30]) /
-                (xCoordinatesList[27] - xCoordinatesList[30]))
-            # since degrees = radians * rad2degCnvtFactor
-            noseBridgeAngleOffset = int(radians1 * rad2degCnvtFactor)
-
-        if noseBridgeAngleOffset < 0:
-            noseBridgeAngleOffset += 90
-        else:
-            noseBridgeAngleOffset -= 90
-            
-        # For each 68 landmark points.
-        for i in range(1, 68):
-            
-            '''
-            cv2.arrowedLine(frame, (int(xCoordMean), int(yCoordMean)), 
-                            (xCoordinatesList[i], yCoordinatesList[i]),
-                            (0, 0, 255), thickness=1, line_type=4,
-                            shift=0, tipLength=0.05)
-            '''
-            '''
-            cv2.circle(frame, (xCoordinatesList[i], yCoordinatesList[i]),
-                       1, (0, 0, 255), thickness=2)
-            cv2.putText(frame, "{}".format(i), (xCoordinatesList[i], yCoordinatesList[i]),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0, 255, 0), thickness=1)
-            '''
-            '''
-            # For each point, draw circle with thickness = 2 on the original frame
-            if i == 27 or i == 30:
-                cv2.circle(frame, (xCoordinatesList[i], yCoordinatesList[i]),
-                       1, (0, 255, 0), thickness=2)
-                cv2.putText(frame, "P{}".format(i), (xCoordinatesList[i], yCoordinatesList[i]),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 255, 0), thickness=1)      
+def main():
+    """
+    Main function - start of the program.
+    """
+    while True:
+        ret, frame = video_capture.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+        clahe_image = clahe.apply(gray)
+        detections = frontalFaceDetector(clahe_image, 1)
+        for d in detections:  # For each detected face
+            shape = facialShapePredictor(clahe_image, d)  # Get coordinates
+            xCoordinatesList = []
+            yCoordinatesList = []
+            # Store the X and Y coordinates of landmark points in two lists
+            for i in range(0, 68):
+                xCoordinatesList.append(shape.part(i).x)
+                yCoordinatesList.append(shape.part(i).y)
+            # Get the mean of both axes to determine centre of gravity
+            xCoordMean = np.mean(xCoordinatesList)
+            yCoordMean = np.mean(yCoordinatesList)
+            if xCoordinatesList[27] == xCoordinatesList[30]:
+                noseBridgeAngleOffset = 0
             else:
-                # pass                
+                radians1 = math.atan(
+                    (yCoordinatesList[27] - yCoordinatesList[30]) /
+                    (xCoordinatesList[27] - xCoordinatesList[30]))
+                # since degrees = radians * RAD2DEG_CVT_FACTOR
+                noseBridgeAngleOffset = int(radians1 * RAD2DEG_CVT_FACTOR)
+            if noseBridgeAngleOffset < 0:
+                noseBridgeAngleOffset += 90
+            else:
+                noseBridgeAngleOffset -= 90
+            # For each 68 landmark points.
+            for i in range(1, 68):
+                '''
+                cv2.arrowedLine(frame, (int(xCoordMean), int(yCoordMean)),
+                                (xCoordinatesList[i], yCoordinatesList[i]),
+                                (0, 0, 255), thickness=1, line_type=4,
+                                shift=0, tipLength=0.05)
+                '''
+                '''
                 cv2.circle(frame, (xCoordinatesList[i], yCoordinatesList[i]),
-                       1, (0, 0, 255), thickness=2)
-            '''
-        # For mean coordinates.
-        cv2.circle(frame, (int(xCoordMean), int(yCoordMean)), 1, (255, 0, 0), thickness=2)
-        cv2.putText(frame, "mean({}, {})".format(int(xCoordMean), int(yCoordMean)), 
-                    (int(xCoordMean), int(yCoordMean)),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255, 0, 0), thickness=1) 
-
-    cv2.imshow("image", frame)  # Display the frame
-    
-    # Save the frame when the user presses 's'
-    if cv2.waitKey(1) & 0xFF == ord('s'):
-        img_name = "..\\img_samples\\img_cap_{}.jpg".format(
-            dt.datetime.today().strftime("%Y%m%d_%H%M%S"))
-        cv2.imwrite(img_name, frame)
-    
-    # Exit program when the user presses 'q'
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+                           1, (0, 0, 255), thickness=2)
+                cv2.putText(frame, "{}".format(i), (xCoordinatesList[i],
+                                                    yCoordinatesList[i]),
+                                                    cv2.FONT_HERSHEY_SIMPLEX,
+                                                    0.3,
+                                                    (0, 255, 0),
+                                                    thickness=1)
+                '''
+                '''
+                # For each point, draw circle with thickness = 2 on the
+                # original frame
+                if i == 27 or i == 30:
+                    cv2.circle(frame, (xCoordinatesList[i],
+                                       yCoordinatesList[i]),
+                                       1,
+                                       (0, 255, 0),
+                                       thickness=2)
+                    cv2.putText(frame,
+                                "P{}".format(i),
+                                (xCoordinatesList[i], yCoordinatesList[i]),
+                                cv2.FONT_HERSHEY_SIMPLEX,
+                                0.4,
+                                (0, 255, 0),
+                                thickness=1)
+                else:
+                    # pass
+                    cv2.circle(frame,
+                               (xCoordinatesList[i], yCoordinatesList[i]),
+                               1,
+                               (0, 0, 255),
+                               thickness=2)
+                '''
+            # For mean coordinates.
+            cv2.circle(frame,
+                       (int(xCoordMean), int(yCoordMean)),
+                       1, (255, 0, 0), thickness=2)
+            cv2.putText(frame, "mean({}, {})".format(int(xCoordMean),
+                                                     int(yCoordMean)),
+                        (int(xCoordMean), int(yCoordMean)),
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        0.4, (255, 0, 0), thickness=1)
+        cv2.imshow("image", frame)  # Display the frame
+        # Save the frame when the user presses 's'
+        if cv2.waitKey(1) & 0xFF == ord('s'):
+            img_name = "..\\img_samples\\img_cap_{}.jpg".format(
+                dt.datetime.today().strftime("%Y%m%d_%H%M%S"))
+            cv2.imwrite(img_name, frame)
+        # Exit program when the user presses 'q'
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
---
+++ src/py_test_trained_svm_model.py
@@ -0,0 +1,197 @@
+"""
+@introduction:
+------------------------------------------------------------------------------
+    Introduction
+    ============
+
+    This module is used to load and test the OpenCV SVM model.
+
+    SVM is short for Support Vector Machine, a Machine Learning algorithm
+    used to classify data but can also be used for regression. I am using it
+    to classify the different states [classes in ML terms] of human face.
+
+    Facial landmarks are extracted using dlib's shape predictor with a
+    pre-trained shape predictor model. These landmarks are further processed
+    as mentioned in the tutorial from Paul Van Gent and fed to the svm.train
+    method.
+
+    I have modified the code used in the tutorial and experimented with
+    simpler and easy to read code. For further description please follow the
+    tutorial by Paul mentioned in the link in README.md of this repo.
+------------------------------------------------------------------------------
+
+@usage:
+------------------------------------------------------------------------------
+    Usage
+    =====
+
+    Run the module as a command line option for python interpreter.
+    -> python py_test_trained_svm_model.py
+------------------------------------------------------------------------------
+
+@purpose:
+------------------------------------------------------------------------------
+    To understand the data, feature generation and representation, SVM model
+    training and SVM prediction.
+------------------------------------------------------------------------------
+
+@based_on:
+------------------------------------------------------------------------------
+    <a href="http://www.paulvangent.com/">
+       Emotion Recognition using Facial Landmarks, Python, DLib and OpenCV
+    </a>
+------------------------------------------------------------------------------
+
+@applications:
+------------------------------------------------------------------------------
+    1. Image information extraction.
+    2. Understanding the machine learning aspects.
+    3. Data extraction and representation.
+------------------------------------------------------------------------------
+"""
+
+# ---------------------------------------------------------------------------
+# Imports
+# ---------------------------------------------------------------------------
+
+import os
+import cv2
+import math
+import dlib
+import numpy as np
+import datetime as dt
+
+# ---------------------------------------------------------------------------
+# Module info
+# ---------------------------------------------------------------------------
+
+__author__    = "Mani Kumar D A - 2017, Paul van Gent - 2016"
+__version__   = "2.1, 24/06/2017"
+__license__   = "GNU GPL v3"
+__copyright__ = "Mani Kumar D A"
+
+# ---------------------------------------------------------------------------
+# Module constants
+# ---------------------------------------------------------------------------
+
+dirsep             = os.sep  # directory path separator
+curdir             = os.curdir  # Relative current directory i.e. '.'
+cwdpath            = os.getcwd()  # current working directory full path name
+
+RAD2DEG_CVT_FACTOR = 180 / math.pi  # Constant to convert radians to degrees.
+emotionsList       = ["anger", "contempt",  # Human facial expression states.
+                      "happy", "neutral", 
+                      "sadness", "surprise"                
+                      ''' 
+                      -> Yet to add `pout`, 
+                      `disgust` and `fear` are excluded
+                      ''']
+
+''' --> For future experiments.
+Training for happy, surprise, neutral.
+emotionsList = ["happy", "surprise", "neutral"]
+'''
+
+claheObject          = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+frontalFaceDetector  = dlib.get_frontal_face_detector()
+facialShapePredictor = dlib.shape_predictor(
+    "..{0}input{1}shape_predictor_68_face_landmarks.dat".format(
+        dirsep, dirsep))
+
+# ---------------------------------------------------------------------------
+# Functions
+# ---------------------------------------------------------------------------
+
+
+def get_landmarks(claheImage):
+    """ Get landmarks from detected faces. """
+    detectedFaces = frontalFaceDetector(claheImage, 1)
+
+    for detectedFace in detectedFaces:
+        xCoordinatesList   = []
+        yCoordinatesList   = []
+        landmarkVectorList = []
+        facialShape = facialShapePredictor(claheImage, detectedFace)
+        # Store the X and Y coordinates of landmark points in two lists
+        for i in range(0, 68):
+            xCoordinatesList.append(facialShape.part(i).x)
+            yCoordinatesList.append(facialShape.part(i).y)
+        # Determine centre of gravity point by calculating the mean of
+        # all points.
+        xCoordMean = np.mean(xCoordinatesList)
+        yCoordMean = np.mean(yCoordinatesList)
+        # Point 27 represents the top of the nose bridge and point 30
+        # represents the tip of the nose - follow the PVG tutorial for
+        # more details on this step.
+        if xCoordinatesList[27] == xCoordinatesList[30]:
+            noseBridgeAngleOffset = 0
+        else:
+            radians1 = math.atan(
+                (yCoordinatesList[27] - yCoordinatesList[30]) /
+                (xCoordinatesList[27] - xCoordinatesList[30]))
+            noseBridgeAngleOffset = int(radians1 * RAD2DEG_CVT_FACTOR)
+        if noseBridgeAngleOffset < 0:
+            noseBridgeAngleOffset += 90
+        else:
+            noseBridgeAngleOffset -= 90
+        for xcoord, ycoord in zip(xCoordinatesList, yCoordinatesList):
+            xyCoordArray = np.asarray((ycoord, xcoord))
+            xyCoordMeanArray = np.asarray((yCoordMean, xCoordMean))
+            pointDistance = np.linalg.norm(xyCoordArray - xyCoordMeanArray)
+            denom = (xcoord - xCoordMean)  # Prevent divide by zero error.
+            if denom == 0:
+                radians2 = 1.5708  # 90 deg = 1.5708 rads
+            else:
+                radians2 = math.atan((ycoord - yCoordMean) / denom)
+            pointAngle = (radians2 * RAD2DEG_CVT_FACTOR) - noseBridgeAngleOffset
+            landmarkVectorList.append(pointDistance)
+            landmarkVectorList.append(pointAngle)
+
+    if len(detectedFaces) < 1:
+        landmarkVectorList = "No face detected!"
+    return landmarkVectorList
+
+
+def main():
+    svm = cv2.SVM()
+    # ------------------------- Loading opencv SVM ------------------------
+    print "\n#################### Loading opencv SVM ####################\n"
+    # Load opencv SVM trained model.
+    svm.load("..\\input\\cv2_svm_happy_surprise.dat")
+    print "Loading opencv SVM model from file - Completed."
+    # ------------------------- Start Webcam ------------------------------
+    video_capture = cv2.VideoCapture(0)  # Webcam object
+    print "\n##################### Starting Webcam ######################\n"
+
+    while True:
+        ret, frame = video_capture.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        clahe_image = claheObject.apply(gray)
+        landmarkVectorList = get_landmarks(clahe_image)
+        if landmarkVectorList == "No face detected!":
+            print "No face detected!, ret is {}".format(ret)
+            break
+        # --------------------- Testing opencv SVM ------------------------
+        # print "\n#################### Testing opencv SVM #######\n"
+        # Testing data must be float32 matrix for the opencv svm.
+        npArrTestData = np.float32(landmarkVectorList)
+
+        result = svm.predict(npArrTestData)
+        # ################### Print result ####################
+        cv2.putText(frame, "You are {}.".format(emotionsList[int(result)]),
+                    (10, 10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 255, 0), thickness=1)
+        cv2.imshow("image", frame)  # Display the frame
+        # Save the frame when the user presses 's'
+        if cv2.waitKey(1) & 0xFF == ord('s'):
+            img_name = "..\\img_samples\\img_cap_{}.jpg".format(
+                dt.datetime.today().strftime("%Y%m%d_%H%M%S"))
+            cv2.imwrite(img_name, frame)
+        # Exit program when the user presses 'q'
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+
+if __name__ == '__main__':
+    main()
+
